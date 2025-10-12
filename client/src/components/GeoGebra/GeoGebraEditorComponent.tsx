ee10d1e45b0f514d4afb0f6bacff135378dca7d5
yawang <@yawang90>
2025-10-12 14:14:28 +0200

    Add Geogebra Applet.

--- client/src/components/GeoGebra/GeoGebraEditorComponent.tsx
+++ client/src/components/GeoGebra/GeoGebraEditorComponent.tsx
@@ -10,20 +10,21 @@ export const GeoGebraEditorComponent = ({ node, updateAttributes }: any) => {
             style={{border: '1px solid #ddd', padding: '8px', borderRadius: '8px', textAlign: 'center', background: '#fafafa',}}>
             <p style={{ fontWeight: 'bold' }}>GeoGebra Applet</p>
 
-            {materialId ? (
+            <input
+                type="text"
+                placeholder="GeoGebra Material ID (z. B. mnb8hv7g)"
+                value={materialId}
+                onChange={(e) => updateAttributes({ materialId: e.target.value })}
+                style={{ width: '80%', padding: '4px', marginBottom: '8px' }}
+            />
+
+            {materialId && (
                 <iframe
                     src={`https://www.geogebra.org/material/iframe/id/${materialId}`}
                     width={width}
                     height={height}
                     style={{ border: 'none' }}
-                    title="GeoGebra Applet"/>
-            ) : (
-                <input
-                    type="text"
-                    placeholder="GeoGebra Material ID (z. B. mnb8hv7g)"
-                    value={materialId}
-                    onChange={(e) => updateAttributes({ materialId: e.target.value })}
-                    style={{ width: '80%', padding: '4px' }}
+                    title="GeoGebra Applet"
                 />
             )}
         </NodeViewWrapper>
