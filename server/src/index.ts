02aa2e4b6b77272bab68c67535988b68abe679b5
yawang <@yawang90>
2026-04-15 11:20:26 +0200

    add it.

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ app.set('trust proxy', 1);
 app.use(express.json({ limit: "5mb" }));
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-        ? ['https://satim-questionnaire-9y9s.onrender.com', 'https://satim-questionnaire-1.onrender.com']
+        ? 'https://satim-questionnaire-1.onrender.com'
         : 'http://localhost:5173',
     credentials: true,
 }));
