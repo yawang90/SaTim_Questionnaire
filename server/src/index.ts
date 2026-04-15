a72a3533374c3cfb530c3e653fd49fd0e40415a1
yawang <@yawang90>
2026-04-15 11:10:44 +0200

    add it.

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.set('trust proxy', 1);
 app.use(express.json({ limit: "5mb" }));
-app.use(cors());
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
         ? ['https://satim-questionnaire-9y9s.onrender.com', 'https://satim-questionnaire-1.onrender.com']
